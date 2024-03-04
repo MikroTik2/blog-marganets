cc5355b506db41ba6f3de0f6464e23ee2744976f
gf2004 <dotsenk20034ua@gmail.com>
2024-03-05 00:05:55 +0200

    test bd

--- server/index.js
+++ server/index.js
@@ -12,76 +12,71 @@ require('dotenv').config({ path: "./config/conf.env" });
 const port = process.env.PORT || process.env.PORT_NODE;
 
 const app = express();
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
 app.get("/", (req, res) => {
      res.send("dfsdfpsdjifs");
 });
 
-app.listen(port, () => {
-     console.log("work");
-})
+const userRoute = require("./routes/userRoute.js");
+const blogRoute = require("./routes/blogRoute.js");
 
-// const userRoute = require("./routes/userRoute.js");
-// const blogRoute = require("./routes/blogRoute.js");
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-// app.use(express.json({ limit: '10mb' }));
-// app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+app.use(morgan("dev"));
+app.use(cors({
+     origin: "http://localhost:5173", 
+     credentials: true, 
+}));
 
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(fileUpload());
-// app.use(morgan("dev"));
-// app.use(cors({
-//      origin: "http://localhost:5173", 
-//      credentials: true, 
-// }));
-
-// mongoose.connect("mongodb+srv://sdfsdgweg2sdgf:OL1WCD74oiOgDJfX@cluster0.dreg8j0.mongodb.net/app").then(() => {
+mongoose.connect("mongodb+srv://sdfsdgweg2sdgf:OL1WCD74oiOgDJfX@cluster0.dreg8j0.mongodb.net/app").then(() => {
      
-//      console.log("Successfully connected to the database");
-//      server.listen(port);
+     console.log("Successfully connected to the database");
      
-//      server.on('error', onError);
-//      server.on('listening', onListening);
+     server.on('error', onError);
+     server.on('listening', onListening);
      
-// }).catch(err => {
-//      console.log("An error occurred while connecting to the database: " + err);
-//      process.exit(1);
-// });
+}).catch(err => {
+     console.log("An error occurred while connecting to the database: " + err);
+     process.exit(1);
+});
 
-// app.get("/", (req, res) => {
-//      res.send("Hello world");
-// });
-
-// cloudinary.config({
-//      cloud_name: process.env.CLOUDINARY_NAME,
-//      api_key: process.env.CLOUDINARY_API_KEY,
-//      api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+cloudinary.config({
+     cloud_name: process.env.CLOUDINARY_NAME,
+     api_key: process.env.CLOUDINARY_API_KEY,
+     api_secret: process.env.CLOUDINARY_API_SECRET,
+});
      
-// app.use("/api/v2", userRoute);
-// app.use("/api/v2", blogRoute);
+app.use("/api/v2", userRoute);
+app.use("/api/v2", blogRoute);
 
-// function onError(error) {
-//      switch (error.code) {
-//           case 'EACCES':
-//                console.log('Port ' + error.port + ' requires elevated privileges');
-//                process.exit(1);
-//                break;
+function onError(error) {
+     switch (error.code) {
+          case 'EACCES':
+               console.log('Port ' + error.port + ' requires elevated privileges');
+               process.exit(1);
+               break;
 
-//           case 'EADDRINUSE':
-//                console.log('Port ' + error.port + ' is already in use');
-//                process.exit(1);
-//                break;
+          case 'EADDRINUSE':
+               console.log('Port ' + error.port + ' is already in use');
+               process.exit(1);
+               break;
 
-//           default:
-//                console.log('An error occurred: ' + error.code);
-//                process.exit(1);
-//      };
-// };
+          default:
+               console.log('An error occurred: ' + error.code);
+               process.exit(1);
+     };
+};
                
-// function onListening() {
-//      console.log('HTTP server listening on port ' + port);
-// };
+function onListening() {
+     console.log('HTTP server listening on port ' + port);
+};
+
+server.listen(port, () => {
+     console.log('work');
+});
