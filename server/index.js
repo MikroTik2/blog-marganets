1331884e5a5207d97b44e75112ddc00845ed236a
gf2004 <dotsenk20034ua@gmail.com>
2024-03-05 10:39:52 +0200

    add hidden menu

--- server/index.js
+++ server/index.js
@@ -30,6 +30,16 @@ app.use(cors({
      credentials: true,
  }));
 
+app.use((req, res, next) => {
+     res.header(
+       "Access-Control-Allow-Origin",
+       "React app URL"
+     );
+     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+     res.header("Access-Control-Allow-Headers", "Content-Type");
+     next();
+});
+
 const dbConnect = () => {
      try {
 
