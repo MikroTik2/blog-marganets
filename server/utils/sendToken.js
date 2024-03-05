bd88328d417ce985168f9a57495d30726c51c386
gf2004 <dotsenk20034ua@gmail.com>
2024-03-05 13:59:50 +0200

    update send token

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -4,6 +4,8 @@ const sendToken = (user, statusCode, res) => {
      const options = {
           expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
           httpOnly: true,
+          sameSite: 'None',  // Add this line
+          secure: true, 
      };
 
      res.status(statusCode).cookie('token', token, options).json({
